e3c8d84d34eef27e3366a7eb023e27c11bfb9fa5
valentinaaattardo <161291505+valentinaaattardo@users.noreply.github.com>
2024-07-25 17:19:04 +0200

    Use a method with a callback

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const figlet = require("figlet");
+const fs = require("fs");
 
-figlet("Ciao Mondo!", function (err, data) {
+figlet("Ciao Mondo!", (err, data) => {
   if (err) {
-    console.log("Si è verificato un errore:");
-    console.dir(err);
+    console.error("Errore nella generazione del testo:", err);
     return;
   }
+
   console.log(data);
+
+  fs.writeFile("art.txt", data, (err) => {
+    if (err) {
+      console.error("Errore nella scrittura del file:", err);
+      return;
+    }
+
+    console.log("File scritto con successo!");
+  });
 });
